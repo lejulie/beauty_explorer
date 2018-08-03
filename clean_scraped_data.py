b516af8f38a1ce5926d4db35c675cf61dd870092
lejulie <julieblairlevine@gmail.com>
2018-08-03 13:24:46 -0400

    update data cleaning script and gitignore

--- clean_scraped_data.py
+++ clean_scraped_data.py
@@ -13,5 +13,14 @@ ulta['ingredients'] = ulta['ingredients'].apply(lambda s: str(s).lower())
 # remove duplicates
 ulta = ulta.drop_duplicates(subset=['product_name','brand_name'])
 
+# remove anything without ingredients, brand name, product name, or URL
+ulta = ulta.dropna(subset=['brand_name', 'product_name', 'ingredients','url'])
+
+# reformat categories
+ulta['categories'] = ulta['categories'].apply(lambda s: s.replace(","," > "))
+
+# convert stars
+ulta['review_avg_rating'] = ulta['review_avg_rating'].apply(lambda s: str(s)[:3] if str(s) != "nan" else "-")
+
 # write to a fresh csv
 ulta.to_csv(sys.argv[2])
